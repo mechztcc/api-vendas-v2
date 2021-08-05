60f5e05cda50fc2cad2232bfd2c8ea87946384c7
mechztcc <dojathan@gmail.com>
2021-08-05 13:02:03 -0300

    Orders routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -6,6 +6,7 @@ import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
 import customersRouter from '@modules/customers/routes/customers.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
@@ -14,6 +15,7 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
-routes.use('/customers', customersRouter);
+routes.use('/customer', customersRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
